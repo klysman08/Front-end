1b0396626f3e4a15003ccfb86af200b871fd18ad
Klysman Rezende <klysman.rk@gmail.com>
2020-04-21 20:46:29 -0300

    up

---
+++ ECMAScript/main.js
@@ -0,0 +1,43 @@
+class Lista {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+
+class TodoList extends Lista {
+    constructor() {
+        super();
+        this.usuario = "Klysman";
+    }
+
+    monstraUsuario(){
+        console.log(this.usuario);
+    }
+
+    static soma (a, b) {
+        return a + b;
+    }
+
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo todo');
+
+}
+
+class Matematica {
+    static soma(a , b) {
+        return a + b;
+    }
+}
+
+MinhaLista.monstraUsuario();
+console.log(Matematica.soma(5, 3));
